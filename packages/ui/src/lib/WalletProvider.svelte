56f5c0d37f58194a1ed57680875ba322ffe61f36
t4top <temitope@yahoo.co.jp>
2023-09-05 08:49:56 +0200

    Temporarily remove un-/register listeners to fix race condition during autoconnect

--- packages/ui/src/lib/WalletProvider.svelte
+++ packages/ui/src/lib/WalletProvider.svelte
@@ -1,30 +1,33 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { WalletReadyState } from '@solana/wallet-adapter-base';
-	import { isWalletAdapterCompatibleWallet, StandardWalletAdapter } from '@solana/wallet-standard-wallet-adapter-base';
+	import { WalletReadyState, isWalletAdapterCompatibleStandardWallet } from '@solana/wallet-adapter-base';
+	import { StandardWalletAdapter } from '@solana/wallet-standard-wallet-adapter-base';
 	import { getWallets } from '@wallet-standard/app';
 	import { initialize } from '@aztemi/svelte-on-solana-wallet-adapter-core';
 	import type { Adapter, WalletError } from '@solana/wallet-adapter-base';
-	import type { Wallet } from '@wallet-standard/base';
 
 	export let localStorageKey: string,
 		wallets: Adapter[] = [],
 		autoConnect = false,
 		onError = (error: WalletError) => console.error(error);
 
-	let nonStandardWallets: Adapter[] = [];
-	let standardWallets: Adapter[] = [];
-
 	$: {
-		const allWallets = [...standardWallets, ...nonStandardWallets];
-		allWallets.sort(installedAdaptersFirst);
-		initialize({ wallets: allWallets, autoConnect, localStorageKey, onError });
-	}
+		if (wallets.length) {
+			// get installed wallets compatible with the standard
+			const { get } = getWallets();
+			const standardWallets = get()
+				.filter(isWalletAdapterCompatibleStandardWallet)
+				.map((wallet) => new StandardWalletAdapter({ wallet }));
 
-	$: {
-		nonStandardWallets = wallets.filter(
-			(wallet) => !standardWallets.some(({ name }) => wallet.name === name)
-		);
+			// filter out non standard wallets
+			const nonStandardWallets = wallets.filter(
+				(wallet) => !standardWallets.some(({ name }) => wallet.name === name)
+			);
+
+			// merge, sort and initialize wallets store
+			const allWallets = [...standardWallets, ...nonStandardWallets];
+			allWallets.sort(installedAdaptersFirst);
+			initialize({ wallets: allWallets, autoConnect, localStorageKey, onError });
+		}
 	}
 
 	function installedAdaptersFirst(a: Adapter, b: Adapter): number {
@@ -35,24 +38,6 @@
 		if (!isInstalled(a) && isInstalled(b)) sort = 1;
 		return sort;
 	}
-
-	function updateStandardWallets(wallets: Wallet[]) {
-		standardWallets = wallets
-			.filter(isWalletAdapterCompatibleWallet)
-			.map((wallet) => new StandardWalletAdapter({ wallet }));
-	}
-
-	onMount(() => {
-		const { get, on } = getWallets();
-		const removeRegisterListener = on('register', () => updateStandardWallets(get()));
-		const removeUnregisterListener = on('unregister', () => updateStandardWallets(get()));
-		updateStandardWallets(get());
-
-		return () => {
-			removeRegisterListener();
-			removeUnregisterListener();
-		};
-	});
 </script>
 
 <svelte:head>
