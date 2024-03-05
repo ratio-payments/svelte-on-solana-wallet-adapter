5389017a06828ece58594e7b856c50e65d78b7c3
t4top <temitope@yahoo.co.jp>
2024-03-05 17:04:30 +0100

    Implement support for Mobile Wallet Adapter

--- packages/ui/src/lib/WalletProvider.svelte
+++ packages/ui/src/lib/WalletProvider.svelte
@@ -2,9 +2,18 @@
 	import { onMount } from 'svelte';
 	import { WalletReadyState, isWalletAdapterCompatibleStandardWallet } from '@solana/wallet-adapter-base';
 	import { StandardWalletAdapter } from '@solana/wallet-standard-wallet-adapter-base';
+	import {
+		createDefaultAddressSelector,
+		createDefaultAuthorizationResultCache,
+		createDefaultWalletNotFoundHandler,
+		SolanaMobileWalletAdapter,
+		SolanaMobileWalletAdapterWalletName
+	} from '@solana-mobile/wallet-adapter-mobile';
+	import { getChainForEndpoint } from '@solana/wallet-standard-util';
 	import { getWallets } from '@wallet-standard/app';
 	import { initialize } from '@aztemi/svelte-on-solana-wallet-adapter-core';
 	import type { Adapter, WalletError } from '@solana/wallet-adapter-base';
+	import { workSpace } from './workSpace';
 
 	export let localStorageKey: string,
 		wallets: Adapter[] = [],
@@ -25,8 +34,13 @@
 			(wallet) => !standardWallets.some(({ name }) => wallet.name === name)
 		);
 
-		// merge, sort and initialize wallets store
 		const allWallets = [...standardWallets, ...nonStandardWallets];
+
+		// merge standard mobile wallet if available
+		const mobileWallet = getMobileWallet(allWallets);
+		if (mobileWallet) allWallets.unshift(mobileWallet);
+
+		// sort and initialize wallets store
 		allWallets.sort(installedAdaptersFirst);
 		initialize({ wallets: allWallets, autoConnect, localStorageKey, onError });
 	}
@@ -38,6 +52,56 @@
 		if (isInstalled(a) && !isInstalled(b)) sort = -1;
 		if (!isInstalled(a) && isInstalled(b)) sort = 1;
 		return sort;
+	}
+
+	function getMobileWallet(wallets: Adapter[]) {
+		/**
+		 * Return null if Mobile wallet adapter is already in the list or if ReadyState is Installed.
+		 *
+		 * There are only two ways a browser extension adapter should be able to reach `Installed` status:
+		 *   1. Its browser extension is installed.
+		 *   2. The app is running on a mobile wallet's in-app browser.
+		 * In either case, we consider the environment to be desktop-like and not 'mobile'.
+		 */
+		if (
+			wallets.some(
+				({ name, readyState }) =>
+					name === SolanaMobileWalletAdapterWalletName || readyState === WalletReadyState.Installed
+			)
+		) {
+			return null;
+		}
+
+		const ua = globalThis.navigator?.userAgent ?? null;
+
+		const isOnAndroid = (ua: string) => /android/i.test(ua);
+
+		const isInWebView = (ua: string) =>
+			/(WebView|Version\/.+(Chrome)\/(\d+)\.(\d+)\.(\d+)\.(\d+)|; wv\).+(Chrome)\/(\d+)\.(\d+)\.(\d+)\.(\d+))/i.test(
+				ua
+			);
+
+		const getUriForAppIdentity = () => {
+			const { location } = globalThis;
+			if (location) return `${location.protocol}//${location.host}`;
+		};
+
+		/**
+		 * Return Mobile Wallet Adapter object if we are running
+		 * on a device that supports MWA like Android
+		 * and we are *not* running in a WebView.
+		 */
+		if (ua && isOnAndroid(ua) && !isInWebView(ua)) {
+			return new SolanaMobileWalletAdapter({
+				addressSelector: createDefaultAddressSelector(),
+				appIdentity: {
+					uri: getUriForAppIdentity()
+				},
+				authorizationResultCache: createDefaultAuthorizationResultCache(),
+				chain: getChainForEndpoint($workSpace?.connection?.rpcEndpoint || ''),
+				onWalletNotFound: createDefaultWalletNotFoundHandler()
+			});
+		}
 	}
 
 	onMount(() => {
